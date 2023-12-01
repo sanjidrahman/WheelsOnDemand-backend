3f9626c512628d8b6057f8db98e1150070b46d7a
Sanjid Rahman <rahmansanjid505@gmail.com>
2023-12-02 00:39:32 +0530

    bug fixes

--- src/main.ts
+++ src/main.ts
@@ -37,7 +37,7 @@ async function bootstrap() {
     }),
   );
   const corsOptions: CorsOptions = {
-    origin: ['http://localhost:4200', 'https://s3.wheelsondemand.online'],
+    origin: ['https://s3.wheelsondemand.online'],
     credentials: true,
   };
   app.enableCors(corsOptions);
