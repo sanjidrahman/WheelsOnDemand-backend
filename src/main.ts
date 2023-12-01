0460032ddcc153b364272cd866c9192f36a6da4d
Sanjid Rahman <rahmansanjid505@gmail.com>
2023-12-02 01:00:55 +0530

    bug fixes

--- src/main.ts
+++ src/main.ts
@@ -36,11 +36,11 @@ async function bootstrap() {
       saveUninitialized: false,
     }),
   );
-  const corsOptions: CorsOptions = {
-    origin: ['https://s3.wheelsondemand.online'],
-    credentials: true,
-  };
-  app.enableCors(corsOptions);
+  // const corsOptions: CorsOptions = {
+  //   origin: ['https://s3.wheelsondemand.online'],
+  //   credentials: true,
+  // };
+  app.enableCors();
   app.use(cookieParser());
   await app.listen(port);
 
