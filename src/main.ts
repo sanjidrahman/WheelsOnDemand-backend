40d63002611b9bcfa18e31b855741e7988e97d2b
Sanjid Rahman <rahmansanjid505@gmail.com>
2023-12-02 11:13:41 +0530

    make changes in main.ts

--- src/main.ts
+++ src/main.ts
@@ -39,7 +39,7 @@ async function bootstrap() {
     }),
   );
   const corsOptions: CorsOptions = {
-    origin: ['https://s3.wheelsondemand.online'],
+    origin: ['http://localhost:4200', 'https://s3.wheelsondemand.online'],
     credentials: true,
   };
   app.enableCors(corsOptions);
